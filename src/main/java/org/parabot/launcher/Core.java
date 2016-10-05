18c8caaf1f6f3f128e89f4ec82e11baade441956
Emmastone <capslock_donater@hotmail.com>
2016-10-05 01:38:44 +0100

    [CLEANUP] Removed debugs at Core

--- src/main/java/org/parabot/launcher/Core.java
+++ src/main/java/org/parabot/launcher/Core.java
@@ -1,28 +1,12 @@
 package org.parabot.launcher;
 
-import org.parabot.launcher.data.Configuration;
-import org.parabot.launcher.io.Downloader;
-
-import java.io.File;
-import java.io.IOException;
-
 /**
  * @author JKetelaar, EmmaStone, Fryslan, Everel
  */
 public class Core {
 
     public static void main(String[] args) {
-        Terminal terminal = new Terminal();
 
-        System.out.println(System.getProperty("java.home"));
 
-        try {
-            if (!new File(Configuration.CLIENT_LOCATION).exists()) {
-                Downloader.downloadFile(Configuration.DOWNLOAD_BOT, Configuration.CLIENT_LOCATION);
-            }
-            terminal.execute();
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 }
