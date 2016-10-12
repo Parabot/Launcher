894753fe2bc5e6a8ba29596d0d1517c6ba310fd2
JKetelaar <jeroen@ketelaar.me>
2016-10-12 20:21:23 +0200

    [CLEANUP/BUGFIX] Reformatted, correct modifiers & removed unused fields

--- src/main/java/org/parabot/launcher/Controller.java
+++ src/main/java/org/parabot/launcher/Controller.java
@@ -13,86 +13,38 @@ import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
-    public static boolean loadLocal;
-    public static boolean noVerify;
-    public static boolean verbose;
-    public static boolean debug;
-    public static boolean server;
-    public static String serverName;
-
     @FXML // fx:id="clearCacheButton"
-    public JFXButton clearCacheButton;
+    private JFXButton clearCacheButton;
 
     @FXML // fx:id="loadLocalToggleButton"
-    public JFXToggleButton loadLocalToggleButton;
+    private JFXToggleButton loadLocalToggleButton;
 
     @FXML // fx:id="versionLabel"
-    public Label versionLabel;
+    private Label versionLabel;
 
     @FXML // fx:id="startButton"
-    public JFXButton startButton;
+    private JFXButton startButton;
 
     @FXML // fx:id="noVerifyToggleButton"
-    public JFXToggleButton noVerifyToggleButton;
+    private JFXToggleButton noVerifyToggleButton;
 
     @FXML // fx:id="verboseToggleButton"
-    public JFXToggleButton verboseToggleButton;
+    private JFXToggleButton verboseToggleButton;
 
     @FXML // fx:id="debugToggleButton"
-    public JFXToggleButton debugToggleButton;
+    private JFXToggleButton debugToggleButton;
 
     @FXML // fx:id="serverTextField"
-    public JFXTextField serverTextField;
+    private JFXTextField serverTextField;
 
     @FXML // fx:id="javaVersionLabel"
-    public Label javaVersionLabel;
+    private Label javaVersionLabel;
 
     @FXML // fx:id="serverToggleButton"
-    public JFXToggleButton serverToggleButton;
+    private JFXToggleButton serverToggleButton;
 
     @FXML // fx:id="statusLabel"
-    public Label statusLabel;
-
-    @FXML
-    void clearCache() {
-        //todo Clear Parabot Cache
-    }
-
-    @FXML
-    void startClient() {
-        //todo Start Parabot Client
-    }
-
-    @FXML
-    void getLoadLocalSelected() {
-        loadLocal = loadLocalToggleButton.isSelected();
-    }
-
-    @FXML
-    void getNoVerifySelected() {
-        noVerify = noVerifyToggleButton.isSelected();
-    }
-
-    @FXML
-    void getVerboseSelected() {
-        verbose = verboseToggleButton.isSelected();
-    }
-
-    @FXML
-    void getDebugSelected() {
-        debug = debugToggleButton.isSelected();
-    }
-
-    @FXML
-    void getServerSelected() {
-        server = serverToggleButton.isSelected();
-        handleServerTextField();
-    }
-
-    @FXML
-    void setServerName() {
-        serverName = serverTextField.getText();
-    }
+    private Label statusLabel;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -110,8 +62,48 @@ public class Controller implements Initializable {
         //todo set Status TextField
     }
 
+    @FXML
+    private void clearCache() {
+        //todo Clear Parabot Cache
+    }
+
+    @FXML
+    private void startClient() {
+        //todo Start Parabot Client
+    }
+
+    @FXML
+    private void getLoadLocalSelected() {
+
+    }
+
+    @FXML
+    private void getNoVerifySelected() {
+
+    }
+
+    @FXML
+    private void getVerboseSelected() {
+
+    }
+
+    @FXML
+    private void getDebugSelected() {
+
+    }
+
+    @FXML
+    private void getServerSelected() {
+        handleServerTextField();
+    }
+
+    @FXML
+    private void setServerName() {
+
+    }
+
     private void handleServerTextField() {
-        if (!server && serverTextField.isVisible()) {
+        if (false && serverTextField.isVisible()) {
             serverTextField.setVisible(false);
         } else {
             serverTextField.setVisible(true);
